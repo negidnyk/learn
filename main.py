a8eda5102d0cae4ca80376df77a457698fdcf2e3
Negidnyk <artem.yavtushieenko@gmail.com>
2023-06-17 17:04:48 +0300

    end of task

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import openai
 from config import OPENAI_API_KEY
 
-
+# get the API key from environment
 openai.api_key = OPENAI_API_KEY
 
-# request = num = input('Enter a request for a picture to be generated: ')
-
+# block of generation of a text according to task
 text = openai.Completion.create(
   model="text-davinci-003",
   prompt="Description of the rubber duck including detailed information about the used materials, design features and purpose",
   max_tokens=150
 )
 
+# receiving a text from response of text generation block
+text_request = text['choices'][0]['text']
+
+# generation of an image according to a response of the generation text block
 response = openai.Image.create(
-  prompt=text['choices'][0]['text'],
+  prompt=text_request,
   n=1,
   size="1024x1024"
 )
+
+# receiving the URL to a generated image
 image_url = response['data'][0]['url']
 
 
-print("Request:\n", text['choices'][0]['text'])
+print("Request:\n", text_request)
 print("\nURL to a generated image:\n", image_url)
